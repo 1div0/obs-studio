f31057e82b17fe9ed5f3e1edef3c58115540908c
jp9000 <obs.jim@gmail.com>
2020-06-20 07:42:14 -0700

    UI: Add virtual camera to UI
    Adds a virtual camera button to the main user interface.  If virtual
    camera is not installed, it will not add the button.  On Windows, it
    detects whether the virtual camera filters are properly registered, and
    will only add the button if the virtual camera filter is confirmed
    registered.
    
    Also adds a virtual camera option to the auto-configuration wizard,
    which will just simply set the user's resolution/scale to 1920x1080 at
    30 FPS.

--- UI/window-basic-main.hpp
+++ UI/window-basic-main.hpp
@@ -168,6 +168,7 @@ class OBSBasic : public OBSMainWindow {
 	friend class ReplayBufferButton;
 	friend class ExtraBrowsersModel;
 	friend class ExtraBrowsersDelegate;
+	friend struct BasicOutputHandler;
 	friend struct OBSStudioAPI;
 
 	enum class MoveDir { Up, Down, Left, Right };
@@ -257,6 +258,9 @@ private:
 	QPointer<QHBoxLayout> replayLayout;
 	QScopedPointer<QPushButton> pause;
 	QScopedPointer<QPushButton> replay;
+
+	QPointer<QPushButton> vcamButton;
+	bool vcamEnabled = false;
 
 	QScopedPointer<QSystemTrayIcon> trayIcon;
 	QPointer<QAction> sysTrayStream;
@@ -384,7 +388,7 @@ private:
 	QModelIndexList GetAllSelectedSourceItems();
 
 	obs_hotkey_pair_id streamingHotkeys, recordingHotkeys, pauseHotkeys,
-		replayBufHotkeys, togglePreviewHotkeys;
+		replayBufHotkeys, vcamHotkeys, togglePreviewHotkeys;
 	obs_hotkey_id forceStreamingStopHotkey;
 
 	void InitDefaultTransitions();
@@ -562,6 +566,12 @@ public slots:
 	void ReplayBufferSave();
 	void ReplayBufferStopping();
 	void ReplayBufferStop(int code);
+
+	void StartVirtualCam();
+	void StopVirtualCam();
+
+	void OnVirtualCamStart();
+	void OnVirtualCamStop(int code);
 
 	void SaveProjectDeferred();
 	void SaveProject();
@@ -742,6 +752,8 @@ public:
 		return os_atomic_load_bool(&previewProgramMode);
 	}
 
+	inline bool VCamEnabled() const { return vcamEnabled; }
+
 	bool StreamingActive() const;
 	bool Active() const;
 
@@ -749,6 +761,7 @@ public:
 	int ResetVideo();
 	bool ResetAudio();
 
+	void AddVCamButton();
 	void ResetOutputs();
 
 	void ResetAudioDevice(const char *sourceId, const char *deviceId,
@@ -889,6 +902,7 @@ private slots:
 
 	void on_streamButton_clicked();
 	void on_recordButton_clicked();
+	void VCamButtonClicked();
 	void on_settingsButton_clicked();
 
 	void on_actionHelpPortal_triggered();
