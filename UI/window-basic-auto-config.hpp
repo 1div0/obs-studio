0c8c31b8eb86bf0e6057444db6848a3f3093b78a
Maya Venkatraman <mvenkat@google.com>
2020-10-12 21:20:12 -0700

    UI: Add support for "More Info" link from service
    (Jim) Allows the ability to get a link from the service's settings about
    a specific service selection the user chooses and display it as a "More
    Info" button that the user can click to find out more information about
    that particular service.

--- UI/window-basic-auto-config.hpp
+++ UI/window-basic-auto-config.hpp
@@ -198,6 +198,7 @@ public slots:
 	void on_useStreamKey_clicked();
 	void ServiceChanged();
 	void UpdateKeyLink();
+	void UpdateMoreInfoLink();
 	void UpdateServerList();
 	void UpdateCompleted();
 };
