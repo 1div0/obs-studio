67649c77a570f8cc80f352f94f0c2c950e893345
jp9000 <obs.jim@gmail.com>
2020-11-01 01:54:27 -0800

    UI: Add VOD track support in advanced output
    Adds a VOD track option (specific to Twitch) that allows a user to
    specify which audio track to use for their Twitch VODs, which uses a
    separate encoder to encode the track. This allows users the ability to
    choose what audio goes on their VOD, separately from the live stream.

--- UI/window-basic-settings.hpp
+++ UI/window-basic-settings.hpp
@@ -32,6 +32,7 @@
 
 class OBSBasic;
 class QAbstractButton;
+class QRadioButton;
 class QComboBox;
 class QCheckBox;
 class QLabel;
@@ -157,6 +158,10 @@ private:
 	uint32_t outputCX = 0;
 	uint32_t outputCY = 0;
 
+	QPointer<QCheckBox> vodTrackCheckbox;
+	QPointer<QWidget> vodTrackContainer;
+	QPointer<QRadioButton> vodTrack[MAX_AUDIO_MIXES];
+
 	void SaveCombo(QComboBox *widget, const char *section,
 		       const char *value);
 	void SaveComboData(QComboBox *widget, const char *section,
@@ -240,6 +245,7 @@ private:
 private slots:
 	void UpdateServerList();
 	void UpdateKeyLink();
+	void UpdateVodTrackSetting();
 	void UpdateMoreInfoLink();
 	void on_show_clicked();
 	void on_authPwShow_clicked();
